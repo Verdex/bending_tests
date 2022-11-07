22cb1176731c51ad3743f377d5b607b34a0cfd52
David L. Inman <davidlevere@gmail.com>
2022-11-06 23:02:37 -0500

    more tests

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,47 @@
 
 use bending::*;
 
-fn main() {
-    /*object_pattern!(_);
-    object_pattern!(5);
-    object_pattern!(5.1);
-    object_pattern!("blah");
-    object_pattern!("blah";5;_);
-    object_pattern!(!; !);*/
-    let output : for<'a> fn(&'a usize) -> Vec<usize> = object_pattern!(!; !; 5 => { 0usize });
 
-    let z = output(&5);
+fn main() {
+    //let _matcher : fn(Tree) -> Vec<char> = object_pattern!( Tree::Leaf(_) => { 's' });
         
-    println!("Hello, world! {:?}", z);
+    println!("Hello, world!");
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    #[derive(Debug)]
+    enum Tree {
+        Node(Box<Tree>, Box<Tree>),
+        Leaf(u8),
+    }
+
+    #[derive(Debug)]
+    enum Options {
+        First,
+        Second,
+    }
+
+    #[test]
+    fn object_pattern_should_handle_namespaceless_cons() {
+        use Options::*;
+        let matcher : fn(Options) -> Vec<char> = object_pattern!( First => { 's' });
+        let output = matcher(First);
+        assert_eq!(output, vec!['s']);
+    }
+
     #[test]
     fn object_pattern_should_handle_single_pattern() {
         let matcher : fn(usize) -> Vec<char> = object_pattern!(5 => { 's' });
+        let output = matcher(5);
+        assert_eq!(output, vec!['s']);
+    }
+
+    #[test]
+    fn object_pattern_should_next_pattern() {
+        let matcher : fn(usize) -> Vec<char> = object_pattern!(!; 5 => { 's' });
         let output = matcher(5);
         assert_eq!(output, vec!['s']);
     }
